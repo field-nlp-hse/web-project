0b05e51074ea3adb6c36c6b5d531f76642a8cf05
alexanderbaranof <alexanderbaranof@gmail.com>
2021-12-18 18:59:09 +0300

    feat: init commit

---
+++ flask_app.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+@app.route("/")
+def home():
+    return render_template('home.html', title = 'Home Page')
+
+if __name__ == '__main__':
+    app.config["SECRET_KEY"] = 'xfnjMCLYhzFUi$4IoQDRe~sSEoe|OprmKGPW68lOgph#Cgty'
+    app.run(debug = True)
