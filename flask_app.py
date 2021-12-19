c121a22d28a92c450253ead441f91a4342acc76d
Emil Nadimanov <enadimanov@whoisblogger.com>
2021-12-19 13:14:56 +0300

    ran linter

--- flask_app.py
+++ flask_app.py
@@ -1,28 +1,32 @@
 import os
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
+
 
 @app.route("/asr")
 def asr():
-    return render_template('asr.html', title = 'FieldNLP ASR')
+    return render_template("asr.html", title="FieldNLP ASR")
+
 
 @app.route("/parsers")
 def parsers():
-    return render_template('parsers.html', title = 'FieldNLP Parsers')
+    return render_template("parsers.html", title="FieldNLP Parsers")
+
 
 @app.route("/dictionaries")
 def dictionaries():
-    return render_template('dictionaries.html', title = 'FieldNLP Dictionaries')
+    return render_template("dictionaries.html", title="FieldNLP Dictionaries")
 
-if __name__ == '__main__':
-    app.config["SECRET_KEY"] = 'xfnjMCLYhzFUi$4IoQDRe~sSEoe|OprmKGPW68lOgph#Cgty'
-    app.run(host='0.0.0.0', debug = True)
+
+if __name__ == "__main__":
+    app.config["SECRET_KEY"] = "xfnjMCLYhzFUi$4IoQDRe~sSEoe|OprmKGPW68lOgph#Cgty"
+    app.run(host="0.0.0.0", debug=True)
