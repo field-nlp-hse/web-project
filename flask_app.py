f4566f5f225a0ecafcd36bbdf080b4fab055dd19
Emil <63649196+EmilNadimanov@users.noreply.github.com>
2021-12-19 15:23:14 +0300

    [8-homepage] clickable header, basic homepage (#7)

--- flask_app.py
+++ flask_app.py
@@ -1,16 +1,32 @@
 import os
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+
 @app.route("/")
 def home():
-    return render_template('home.html', title = 'Home Page')
+    return render_template("home.html")
 
-if __name__ == '__main__':
-    app.config["SECRET_KEY"] = 'xfnjMCLYhzFUi$4IoQDRe~sSEoe|OprmKGPW68lOgph#Cgty'
-    app.run(host='0.0.0.0', debug = True)
+
+@app.route("/asr")
+def asr():
+    return render_template("asr.html", title="FieldNLP ASR")
+
+
+@app.route("/parsers")
+def parsers():
+    return render_template("parsers.html", title="FieldNLP Parsers")
+
+
+@app.route("/dictionaries")
+def dictionaries():
+    return render_template("dictionaries.html", title="FieldNLP Dictionaries")
+
+
+if __name__ == "__main__":
+    app.config["SECRET_KEY"] = "xfnjMCLYhzFUi$4IoQDRe~sSEoe|OprmKGPW68lOgph#Cgty"
+    app.run(host="0.0.0.0", debug=True)
